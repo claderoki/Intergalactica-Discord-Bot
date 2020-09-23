20460cb3f880ca8f31d5d3871a01d9b7c351ff42
Claderoki <c.derhaag@protonmail.com>
2020-09-23 11:26:54 +0200

    added purge introduction command

---
+++ src/discord/cogs/intergalactica.py
@@ -0,0 +1,39 @@
+import asyncio
+
+import discord
+from discord.ext import commands
+
+# from src.models import Human, database as db
+# import src.config as config
+
+class Intergalactica(commands.Cog):
+
+    def __init__(self, bot):
+        super().__init__()
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        # self.poll.start()
+        pass
+
+    @commands.command()
+    @commands.has_guild_permissions(administrator = True)
+    async def purgeintros(self, ctx):
+        channel = ctx.guild.get_channel(742567349613232249)
+
+        coros = []
+        async for message in channel.history(limit=200):
+            if not isinstance(message.author, discord.Member):
+                embed = discord.Embed(
+                    color = ctx.guild_color,
+                    title = f"Introduction by {message.author}",
+                    description = message.content)
+                coros.append( ctx.send(embed = embed) )
+
+                await message.delete()
+
+        asyncio.gather(*coros)
+
+def setup(bot):
+    bot.add_cog(Intergalactica(bot))
