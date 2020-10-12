daac139a87709ef0a8aaa0fb5896686c7228589c
Claderoki <c.derhaag@protonmail.com>
2020-10-12 20:47:30 +0200

    base game where all the games in this bot should derive from

---
+++ src/games/game/base.py
@@ -0,0 +1,61 @@
+from src.models import Human, database
+
+class BaseGame:
+    def __init__(self, players, ui, bet):
+        self.players = players
+        self.ui = ui
+        self.bet = bet
+
+    def __init_subclass__(cls):
+        assert hasattr(cls, "start")
+        assert hasattr(cls, "stop")
+
+
+
+class BaseUi:
+    pass
+
+class DiscordUi(BaseUi):
+    pass
+
+class BasePlayer:
+    def __init__(self, identity):
+        self.identity = identity
+
+
+class BaseIdentity:
+    def __init_subclass__(cls):
+        assert hasattr(cls, "add_points")
+        assert hasattr(cls, "remove_points")
+
+class AiIdentity(BaseIdentity):
+    def __init__(self, name = None):
+        self.name = name
+
+    def __str__(self):
+        return str(self.name)
+
+    def add_points(self, points):
+        pass
+
+    def remove_points(self, points):
+        pass
+
+class DiscordIdentity(BaseIdentity):
+    def __init__(self, member):
+        self.member = member
+
+    def __str__(self):
+        return self.member.name
+        # return str(self.member)
+
+    def add_points(self, points):
+        with database:
+            human, _ = Human.get_or_create_for_member(self.member)
+            human.gold += points
+
+    def remove_points(self, points):
+        with database:
+            human, _ = Human.get_or_create_for_member(self.member)
+            human.gold -= points
+
