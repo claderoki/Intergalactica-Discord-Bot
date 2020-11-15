7135afc3aeae2b4e47e865e3faf6e1477b10a970
Claderoki <c.derhaag@protonmail.com>
2020-11-15 21:26:53 +0100

    added intwaiter for waiter_for

--- src/models/base.py
+++ src/models/base.py
@@ -41,6 +41,8 @@ class BaseModel(peewee.Model):
             return EnumWaiter(ctx, field.enum, **kwargs)
         if isinstance(field, (peewee.TextField, peewee.CharField)):
             return StrWaiter(ctx, max_words = None, **kwargs)
+        if isinstance(field, (peewee.IntegerField, peewee.BigIntegerField)):
+            return IntWaiter(ctx, **kwargs)
 
     async def editor_for(self, attr, ctx, on_skip = "pass", **kwargs):
         waiter = self.waiter_for(attr, ctx, **kwargs)
