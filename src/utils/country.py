8d6931c32f643c24b305ca23c6ddc26c91930810
Claderoki <c.derhaag@protonmail.com>
2020-11-18 18:18:46 +0100

    currencies converted to pycountries.currency objects, checks if country exists, and throws an exception if it doesn't.

--- src/utils/country.py
+++ src/utils/country.py
@@ -4,9 +4,14 @@ from emoji import emojize
 import pycountry
 from countryinfo import CountryInfo
 
+class CountryNotFound(Exception):
+    pass
+
 class Country(CountryInfo):
     def __init__(self, alpha_2):
         super().__init__(alpha_2)
+        if super().name() == alpha_2:
+            raise CountryNotFound("Not found.")
         self._country = pycountry.countries.get(alpha_2 = alpha_2)
 
     def name(self):
@@ -14,6 +19,9 @@ class Country(CountryInfo):
 
     def languages(self):
         return [pycountry.languages.get(alpha_2 = x) for x in super().languages()]
+
+    def currencies(self):
+        return [pycountry.currencies.get(alpha_2 = x) for x in super().currencies()]
 
     @property
     def alpha_2(self):
@@ -31,7 +39,7 @@ class Country(CountryInfo):
 
     @property
     def flag_emoji(self):
-        return "".join([emojize(f":regional_indicator_symbol_letter_{x}:") for x in self.alpha_2.lower()])
+        return "".join(emojize(f":regional_indicator_symbol_letter_{x}:") for x in self.alpha_2.lower())
 
     @classmethod
     def random(cls):
@@ -40,15 +48,12 @@ class Country(CountryInfo):
         while alpha_2 is None:
             alpha_2 = random.choice(countries).alpha_2
             try:
-                CountryInfo(alpha_2).capital()
                 CountryInfo(alpha_2).capital_latlng()
             except KeyError:
                 alpha_2 = None
 
         return cls.from_alpha_2(alpha_2)
 
-
 if __name__ == "__main__":
-    country = Country.from_alpha_2("nl")
+    country = Country.from_alpha_2("efw")
     print(country.name())
-    print(country.languages())
