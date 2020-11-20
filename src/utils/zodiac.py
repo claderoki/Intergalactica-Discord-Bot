cef547d1bff5e8c7242f6eef0fc5a0aa6ed13c16
Claderoki <c.derhaag@protonmail.com>
2020-11-20 20:14:59 +0100

    made zodiac sign class a bit cleaner. Also added a DateRange class.

--- src/utils/zodiac.py
+++ src/utils/zodiac.py
@@ -1,82 +1,81 @@
+from enum import Enum
+import datetime
 
-class ZodiacSign:
-    __slots__ = ("name", "emoji")
+from emoji import emojize
 
-    def __init__(self, name, emoji):
-        self.name = name
-        self.emoji = emoji
+class DateRange:
+    def __init__(self, start, stop = None, step = datetime.timedelta(days = 1)):
+        self.start      = start
+        self.stop       = stop or datetime.datetime.utcnow()
+        self.step       = step
+
+        self.validate()
+
+    def validate(self):
+        assert self.start < self.stop
+        assert isinstance(self.start, (datetime.datetime, datetime.date))
+        assert isinstance(self.stop, (datetime.datetime, datetime.date))
+        assert isinstance(self.step, datetime.timedelta)
+
+    def __iter__(self):
+        date = self.start
+        while date < self.stop:
+            yield date
+            date += self.step
+
+    def __contains__(self, date):
+        return date >= self.start and date <= self.stop
+
+class ZodiacSign(Enum):
+    sagittarius = 0
+    capricorn   = 1
+    aquarius    = 2
+    pisces      = 3
+    aries       = 4
+    taurus      = 5
+    gemini      = 6
+    cancer      = 7
+    leo         = 8
+    virgo       = 9
+    libra       = 10
+    scorpio     = 11
+
+    @property
+    def emoji(self):
+        return emojize(f":{self.name.title()}:")
 
     @classmethod
     def from_date(cls, date):
         if date.month == 12:
-            return cls.sagittarius() if (date.day < 22) else cls.capricorn()
+            return cls.sagittarius if (date.day < 22) else cls.capricorn
         elif date.month == 1:
-            return cls.capricorn() if (date.day < 20) else cls.aquarius()
+            return cls.capricorn if (date.day < 20) else cls.aquarius
         elif date.month == 2:
-            return cls.aquarius() if (date.day < 19) else cls.pisces()
+            return cls.aquarius if (date.day < 19) else cls.pisces
         elif date.month == 3:
-            return cls.pisces() if (date.day < 21) else cls.aries()
+            return cls.pisces if (date.day < 21) else cls.aries
         elif date.month == 4:
-            return cls.aries() if (date.day < 20) else cls.taurus()
+            return cls.aries if (date.day < 20) else cls.taurus
         elif date.month == 5:
-            return cls.taurus() if (date.day < 21) else cls.gemini()
+            return cls.taurus if (date.day < 21) else cls.gemini
         elif date.month == 6:
-            return cls.gemini() if (date.day < 21) else cls.cancer()
+            return cls.gemini if (date.day < 21) else cls.cancer
         elif date.month == 7:
-            return cls.cancer() if (date.day < 23) else cls.leo()
+            return cls.cancer if (date.day < 23) else cls.leo
         elif date.month == 8:
-            return cls.leo() if (date.day < 23) else cls.virgo()
+            return cls.leo if (date.day < 23) else cls.virgo
         elif date.month == 9:
-            return cls.virgo() if (date.day < 23) else cls.libra()
+            return cls.virgo if (date.day < 23) else cls.libra
         elif date.month == 10:
-            return cls.libra() if (date.day < 23) else cls.scorpio()
+            return cls.libra if (date.day < 23) else cls.scorpio
         elif date.month == 11:
-            return cls.scorpio() if (date.day < 22) else cls.sagittarius()
+            return cls.scorpio if (date.day < 22) else cls.sagittarius
 
-    @classmethod
-    def sagittarius(cls):
-        return cls("sagittarius", "♐")
+if __name__ == "__main__":
+    dates = DateRange(datetime.datetime(2020, 11, 10))
+    for date in dates:
+        print(date)
+    print(datetime.datetime(2020, 11, 10) in dates)
 
-    @classmethod
-    def capricorn(cls):
-        return cls("capricorn", "♑")
-
-    @classmethod
-    def aquarius(cls):
-        return cls("aquarius", "♒")
-
-    @classmethod
-    def pisces(cls):
-        return cls("pisces", "♓")
-
-    @classmethod
-    def aries(cls):
-        return cls("aries", "♈")
-
-    @classmethod
-    def taurus(cls):
-        return cls("taurus", "♉")
-
-    @classmethod
-    def gemini(cls):
-        return cls("gemini", "♊")
-
-    @classmethod
-    def cancer(cls):
-        return cls("cancer", "♋")
-
-    @classmethod
-    def leo(cls):
-        return cls("leo", "♌")
-
-    @classmethod
-    def virgo(cls):
-        return cls("virgo", "♍")
-
-    @classmethod
-    def libra(cls):
-        return cls("libra", "♎")
-
-    @classmethod
-    def scorpio(cls):
-        return cls("scorpio", "♏")
+    sign = ZodiacSign.from_date(datetime.datetime(2020, 11, 10))
+    print(sign.emoji)
