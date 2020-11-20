162d1a3c42861104211d1e30d2667fd9bba34acb
Clark Derhaag <1850598derhaag@zuyd.nl>
2020-11-20 22:39:31 +0100

    working on emoji commands some more

--- src/discord/cogs/admin.py
+++ src/discord/cogs/admin.py
@@ -14,6 +14,10 @@ class Admin(discord.ext.commands.Cog):
         super().__init__()
         self.bot = bot
 
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.guilds = [x for x in self.bot.guilds if x.owner_id == self.bronk_id]
+
     @commands.is_owner()
     @commands.group()
     async def tester(self, ctx):
@@ -35,16 +39,30 @@ class Admin(discord.ext.commands.Cog):
 
     @commands.group()
     @commands.is_owner()
-    async def emoji():
+    async def emoji(self, ctx):
         pass
 
-    @emoji.command()
-    @commands.is_owner()
-    async def add(self, ctx, name):
+    @emoji.command(name = "add")
+    async def emoji_add(self, ctx, name):
         if len(ctx.message.attachments) == 0:
             raise SendableException(ctx.translate("no_attachments"))
+        image = ctx.message.attachments[0]
 
-        print()
+        available_guilds = [x for x in self.guilds if len(x.emojis) < x.emoji_limit]
+        guild = available_guilds[0]
+        # guild = ctx.guild
+        emoji = await guild.create_custom_emoji(name = name, image = await image.read())
+        SavedEmoji.create(name = emoji.name, guild_id = guild.id, emoji_id = emoji.id)
+        asyncio.gather(ctx.send(ctx.translate("emoji_created")))
+
+    @emoji.command(name = "remove")
+    async def emoji_remove(self, ctx, name):
+        try:
+            saved_emoji = SavedEmoji.get(name = name)
+        except SavedEmoji.DoesNotExist:
+            return
+
+        asyncio.gather(ctx.send(ctx.translate("emoji_not_removed_yet_because_not_actually_working")))
 
 def setup(bot):
     bot.add_cog(Admin(bot))
