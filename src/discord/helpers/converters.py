43918cf3feca9f09bd9ba85679cbd2b7f4038b6c
Claderoki <c.derhaag@protonmail.com>
2020-11-29 00:24:29 +0100

    added a convert_to_time converter.

--- src/discord/helpers/converters.py
+++ src/discord/helpers/converters.py
@@ -3,6 +3,32 @@ from discord.ext import commands
 
 from src.discord.errors.base import SendableException
 
+def convert_to_time(argument):
+    argument = argument.lower()
+
+    pm = "pm" in argument
+    argument = argument.replace("pm","").replace("am", "")
+
+    if ":" in argument:
+        try:
+            hour, minute = argument.split(":")
+        except:
+            return None
+    elif argument.isdigit():
+        hour = argument
+        minute = 0
+    else:
+        return None
+
+    hour = int(hour)
+    minute = int(minute)
+    if pm:
+        hour += 12
+        if hour >= 24:
+            hour = 0
+
+    return datetime.time(hour = hour, minute = minute)
+
 def convert_to_date(argument):
     text = argument
 
