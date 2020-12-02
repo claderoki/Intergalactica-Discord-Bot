c3772793bb6af6693c6cf795dba7807c190169c7
Claderoki <c.derhaag@protonmail.com>
2020-12-02 17:05:54 +0100

    added a location model.

--- src/models/admin.py
+++ src/models/admin.py
@@ -1,3 +1,5 @@
+import datetime
+
 import peewee
 
 from .base import BaseModel
@@ -6,3 +8,14 @@ class SavedEmoji(BaseModel):
     name        = peewee.CharField        (null = False, unique = True)
     guild_id    = peewee.BigIntegerField  (null = False)
     emoji_id    = peewee.BigIntegerField  (null = False)
+
+class Location(BaseModel):
+    latitude   = peewee.DecimalField  (null = False)
+    longitude  = peewee.DecimalField  (null = False)
+    created_on = peewee.DateTimeField (null = True, default = lambda : datetime.datetime.utcnow())
+    name       = peewee.TextField     (null = False)
+
+    @property
+    def google_maps_url(self):
+        return f"https://www.google.com/maps/search/?api=1&query={self.latitude},{self.longitude}"
+# 
