7c07dbad275b49aad0c9b4a845c4c9bdf3a51130
Claderoki <c.derhaag@protonmail.com>
2020-12-19 01:42:41 +0100

    added personalquestion model.

--- src/models/admin.py
+++ src/models/admin.py
@@ -1,6 +1,7 @@
 import datetime
 
 import peewee
+import discord
 
 from .base import BaseModel, EmojiField
 
@@ -20,9 +21,21 @@ class Location(BaseModel):
         return f"https://www.google.com/maps/place/{self.latitude}+{self.longitude}/@{self.latitude},{self.longitude},20z"
 
 class DailyReminder(BaseModel):
-    time       = peewee.TimeField       (null = False)
-    text       = EmojiField             (null = False)
-    weekend    = peewee.BooleanField    (null = True)
-    weekday    = peewee.BooleanField    (null = True)
-    user_id    = peewee.BigIntegerField (null = False)
-    last_reminded = peewee.DateField    (null = True)
+    time          = peewee.TimeField       (null = False)
+    text          = EmojiField             (null = False)
+    weekend       = peewee.BooleanField    (null = True)
+    weekday       = peewee.BooleanField    (null = True)
+    user_id       = peewee.BigIntegerField (null = False)
+    last_reminded = peewee.DateField       (null = True)
+
+class PersonalQuestion(BaseModel):
+    value = peewee.TextField    (null = False)
+    asked = peewee.BooleanField (null = False, default = False)
+
+    @classmethod
+    def get_random(cls):
+        return cls.select().where(cls.asked == False).order_by(peewee.fn.Rand()).first()
+
+    @property
+    def embed(self):
+        return discord.Embed(title = "Question", description = self.value)
