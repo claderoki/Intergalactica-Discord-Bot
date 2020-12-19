63b393db78c3444bf565c6964d68bb174fdc9282
Claderoki <c.derhaag@protonmail.com>
2020-12-19 01:42:57 +0100

    added all the models to init

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,13 +1,13 @@
 from .base import BaseModel
 from .human import Human, Item, HumanItem
-from .intergalactica import Earthling, TemporaryChannel
+from .intergalactica import Earthling, TemporaryChannel, RedditAdvertisement
 from .analytics import EmojiUsage
 from .settings import Settings, NamedEmbed, NamedChannel, Translation, Locale
 from .ticket import Ticket, Reply
 from .poll import Change, Parameter, Poll, PollTemplate, Vote, Option
 from .scene import Scene, Scenario
 from .pigeon import Pigeon, Fight, Exploration, Mail, LanguageMastery, SystemMessage, Date
-from .admin import SavedEmoji, Location, DailyReminder
+from .admin import SavedEmoji, Location, DailyReminder, PersonalQuestion
 from .prank import NicknamePrank, Prankster
 from .reddit import Subreddit
 
@@ -15,11 +15,11 @@ database = Human._meta.database
 
 def setup():
     with database.connection_context():
-        # database.drop_tables([Pigeon, Fight, Exploration, Mail, LanguageMastery, SystemMessage, Date ])
-        database.create_tables([Pigeon, Fight, Exploration, Mail, LanguageMastery, SystemMessage, Date ])
+        # database.drop_tables([Pigeon, Fight, Exploration, Mail, LanguageMastery, SystemMessage, Date])
+        database.create_tables([Pigeon, Fight, Exploration, Mail, LanguageMastery, SystemMessage, Date])
 
-        # database.drop_tables([SavedEmoji, Location, DailyReminder])
-        database.create_tables([SavedEmoji, Location, DailyReminder])
+        # database.drop_tables([SavedEmoji, Location, DailyReminder, PersonalQuestion])
+        database.create_tables([SavedEmoji, Location, DailyReminder, PersonalQuestion])
 
         # database.drop_tables([Subreddit])
         database.create_tables([Subreddit])
@@ -33,8 +33,8 @@ def setup():
         # database.drop_tables([Human, Item, HumanItem])
         database.create_tables([Human, Item, HumanItem])
 
-        # database.drop_tables([Earthling, TemporaryChannel])
-        database.create_tables([Earthling, TemporaryChannel])
+        # database.drop_tables([Earthling, TemporaryChannel, RedditAdvertisement])
+        database.create_tables([Earthling, TemporaryChannel, RedditAdvertisement])
 
         # database.drop_tables([Settings, NamedEmbed, NamedChannel, Locale, Translation])
         database.create_tables([Settings, NamedEmbed, NamedChannel, Locale, Translation])
