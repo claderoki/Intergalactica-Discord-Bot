484fc8eaf04f4ec354f9cfa2cda20aabf5e5d708
Claderoki <c.derhaag@protonmail.com>
2021-01-06 23:39:56 +0100

    added html to discord converter (example: <i>hi</i> will become *hi*

--- src/utils/general.py
+++ src/utils/general.py
@@ -26,3 +26,17 @@ def text_to_emojis(text):
         emojis.append(emoji.emojize(emoji_format.format(char = char), use_aliases=True))
 
     return emojis
+
+def html_to_discord(text):
+    tags = {
+        "i"      : "*",
+        "strong" : "**"
+    }
+
+    for tag, replacement in tags.items():
+        text = text.replace(f"<{tag}>", replacement)
+        text = text.replace(f"</{tag}>", replacement)
+
+    return text
+
+
