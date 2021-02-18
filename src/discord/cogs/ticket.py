6f49b5492119454570f2f137fd392b2b2d894d34
Claderoki <c.derhaag@protonmail.com>
2021-02-18 23:39:10 +0100

    ticket cleanup

--- src/discord/cogs/ticket.py
+++ src/discord/cogs/ticket.py
@@ -17,21 +17,6 @@ class TicketCog(BaseCog, name = "Ticket"):
     async def on_ready(self):
         pass
 
-    async def create_ticket(self, user, guild, type):
-        guild = self.bot.get_guild(742146159711092757)
-        member = guild.get_member(ctx.author.id)
-
-        if member is None:
-            return
-
-        channel = guild.get_channel(758296826549108746)
-
-        anonymous = True
-        ticket = Ticket.create(text = concern, user_id = member.id, anonymous = anonymous, type = Ticket.Type.concern, guild_id = guild.id)
-        await ticket.sync_message(channel)
-
-
-
     @commands.command()
     @commands.dm_only()
     async def concern(self, ctx, *, concern):
@@ -40,11 +25,18 @@ class TicketCog(BaseCog, name = "Ticket"):
 
         if member is None:
             return
-
+        #TODO: hardcoded channel.
         channel = guild.get_channel(758296826549108746)
 
         anonymous = True
-        ticket = Ticket.create(text = concern, user_id = member.id, channel_id = channel.id, anonymous = anonymous, type = Ticket.Type.concern, guild_id = guild.id)
+        ticket = Ticket.create(
+            text       = concern,
+            user_id    = member.id,
+            channel_id = channel.id,
+            anonymous  = anonymous,
+            type       = Ticket.Type.concern,
+            guild_id   = guild.id
+        )
         await ticket.sync_message(channel)
 
     @commands.command()
@@ -69,19 +61,33 @@ class TicketCog(BaseCog, name = "Ticket"):
             for attachment in attachments:
                 response += f"\n{attachment.url}"
 
-        reply = Reply.create(anonymous = ticket.anonymous, user_id = ctx.author.id, text = response, ticket = ticket, type = type)
+        Reply.create(
+            anonymous = ticket.anonymous,
+            user_id   = ctx.author.id,
+            text      = response,
+            ticket    = ticket,
+            type      = type
+        )
         await ticket.sync_message()
 
     @commands.has_guild_permissions(administrator = True)
     @commands.command()
-    async def close(self, ctx, ticket : Ticket, *, text = None):
+    async def close(self, ctx, ticket : Ticket, *, response = None):
         attachments = ctx.message.attachments
         if len(attachments) > 0:
+            response = ""
             for attachment in attachments:
                 response += f"\n{attachment.url}"
 
-        if text:
-            reply = Reply.create(anonymous = ticket.anonymous, user_id = ctx.author.id, text = text, ticket = ticket, type = Reply.Type.staff)
+        if response:
+            Reply.create(
+                anonymous = ticket.anonymous,
+                user_id   = ctx.author.id,
+                text      = response,
+                ticket    = ticket,
+                type      = Reply.Type.staff
+            )
+
         ticket.status = Ticket.Status.closed
         ticket.close_reason = Ticket.CloseReason.resolved
         await ticket.sync_message()
