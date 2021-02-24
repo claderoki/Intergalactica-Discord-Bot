b2513b34a46fc721985fbe01618968eeaf6bc4a0
Clark Derhaag <1850598derhaag@zuyd.nl>
2021-02-24 23:11:53 +0100

    harvesting and planting will cost / give items now.

--- src/discord/cogs/farming.py
+++ src/discord/cogs/farming.py
@@ -10,7 +10,7 @@ from PIL import Image
 
 import src.config as config
 from src.discord.cogs.core import BaseCog
-from src.models import Farm, Crop, Human, FarmCrop
+from src.models import Farm, Crop, FarmCrop, Human, HumanItem
 from src.discord.helpers.converters import StringConverter
 from src.discord.errors.base import SendableException
 
@@ -69,7 +69,16 @@ class FarmingCog(BaseCog, name = "Farming"):
             raise SendableException(ctx.translate("already_planted"))
 
         crop = Crop.get(name = crop_name)
+        item = crop.seed_item
+
+        human_item = HumanItem.get_or_none(item = item, human = ctx.get_human())
+        if human_item is None or human_item.amount <= 0:
+            raise SendableException(ctx.translate("item_not_enough").format(amount = 1))
+
         ctx.farm.plant(crop = crop)
+
+        human_item.amount -= 1
+        human_item.save()
 
         await ctx.success(ctx.translate("crop_planted").format(crop = crop))
 
@@ -80,9 +89,19 @@ class FarmingCog(BaseCog, name = "Farming"):
             raise SendableException(ctx.translate("nothing_to_harvest"))
         farm_crop.finished = True
         farm_crop.save()
+        crop = farm_crop.crop
+        item = crop.product_item
 
-        await ctx.success(ctx.translate("crop_harvested").format(crop = farm_crop.crop))
+        human_item, _   = HumanItem.get_or_create(item = item, human = ctx.get_human())
+        products_gained = random.randint(2, 6)
 
+        human_item.amount += products_gained
+        human_item.save()
+
+        embed = discord.Embed(color = discord.Color.green())
+        embed.description = f"You have successfully harvested {products_gained} {item.name}s!"
+        embed.set_image(url = item.image_url)
+        await ctx.send(embed = embed)
 
 def setup(bot):
     bot.add_cog(FarmingCog(bot))
