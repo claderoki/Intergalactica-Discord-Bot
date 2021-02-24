b467603660f78265d15c41c3c5aceca57391d4a7
Claderoki <c.derhaag@protonmail.com>
2021-02-24 02:25:31 +0100

    conversations cleanup

--- src/discord/cogs/conversations.py
+++ src/discord/cogs/conversations.py
@@ -11,6 +11,27 @@ from src.models import Conversant, Conversation, database
 from src.discord.errors.base import SendableException
 from src.discord.cogs.core import BaseCog
 
+async def check_if_available(user):
+    def check(message):
+        if message.author.id != user.id:
+            return False
+        if not isinstance(message.channel, discord.DMChannel):
+            return False
+        if message.content.lower() in ("no", "n"):
+            return False
+        if message.content.lower() in ("yes", "y"):
+            return True
+        return True
+
+    await user.send("Are you available to talk? (yes | no)")
+    try:
+        await config.bot.wait_for("message", check = check, timeout = 60)
+        return True
+    except asyncio.TimeoutError:
+        await user.send("You are clearly not available to talk.")
+        return False
+
+
 def is_command(message):
     bot = config.bot
     prefix = bot.command_prefix
@@ -42,17 +63,19 @@ class ConversationsCog(BaseCog, name = "Conversations"):
         if is_command(message):
             return
 
-        conversant, created = Conversant.get_or_create(user_id = message.author.id)
-        if created:
+        try:
+            conversant = Conversant.get(user_id = message.author.id)
+        except Conversant.DoesNotExist:
             return
 
         conversation = Conversation.select_for(conversant, finished = False).first()
 
         if conversation is not None:
-            other = conversation.conversant2 if conversation.conversant1 == conversant else conversation.conversant1
+            other = conversation.get_other_conversant(conversant)
             await other.user.send(message.content)
 
     @commands.group()
+    @commands.dm_only()
     async def conversation(self, ctx):
         pass
 
@@ -64,6 +87,21 @@ class ConversationsCog(BaseCog, name = "Conversations"):
         conversant.save()
         await ctx.success(ctx.translate("conversations_toggled_" + ("on" if conversant.enabled else "off")))
 
+    @conversation.command(name = "end")
+    async def conversation_end(self, ctx):
+        conversant, _ = Conversant.get_or_create(user_id = ctx.author.id)
+        conversation = Conversation.select_for(conversant, finished = False).first()
+        if conversation is None:
+            raise SendableException(ctx.translate("no_running_conversation"))
+
+        conversation.end_time = datetime.datetime.utcnow()
+        conversation.finished = True
+        conversation.save()
+
+        other = conversation.get_other_conversant(conversant)
+        for user in (ctx.author, other.user):
+            await user.send(ctx.translate("conversation_ended"))
+
     @conversation.command(name = "start")
     async def conversation_start(self, ctx):
         conversant, _ = Conversant.get_or_create(user_id = ctx.author.id)
@@ -74,43 +112,24 @@ class ConversationsCog(BaseCog, name = "Conversations"):
 
         conversant.enabled = True
         conversant.save()
-        query = Conversant.select(Conversant.user_id)
-        #TODO: any conversant currently not already in a conversation
-        query = query.where(Conversant.enabled == True)
-        query = query.where(Conversant.id != conversant.id)
-        user_ids = [x.user_id for x in query]
+
+        user_ids = [x.user_id for x in Conversant.get_available()]
         if len(user_ids) == 0:
-            raise SendableException(ctx.translate("no_conversants_found"))
+            raise SendableException(ctx.translate("no_conversants_available"))
+
+        await ctx.success(ctx.translate("attempting_to_find_conversant"))
 
         random.shuffle(user_ids)
 
         user_to_speak_to = None
-
         for user_id in user_ids:
             user = self.bot.get_user(user_id)
-
-            def check(message):
-                if message.author.id != user.id:
-                    return False
-                if not isinstance(message.channel, discord.DMChannel):
-                    return False
-                if message.content.lower() in ("no", "n"):
-                    return False
-                if message.content.lower() in ("yes", "y"):
-                    return True
-                return True
-
-            await user.send("Are you available to talk? (yes | no)")
-            try:
-                await self.bot.wait_for("message", check = check, timeout = 60)
-            except asyncio.TimeoutError:
-                await user.send("You are clearly not available to talk.")
-                continue
-            user_to_speak_to = user
-            break
+            if await check_if_available(user):
+                user_to_speak_to = user
+                break
 
         if user_to_speak_to is None:
-            return
+            raise SendableException(ctx.translate("no_conversants_available"))
 
         conversation = Conversation()
         conversation.conversant1 = conversant
@@ -126,7 +145,7 @@ class ConversationsCog(BaseCog, name = "Conversations"):
         embed.description = "\n".join(lines)
 
         i = 1
-        for user in (user_to_speak_to, self.bot.get_user(conversant.user_id)):
+        for user in (user_to_speak_to, ctx.author):
             id = getattr(conversation, f"conversant{i}_key")
             embed.set_footer(text = f"Speaking to conversant with id '{id}'")
             await user.send(embed = embed)
--- src/models/conversation.py
+++ src/models/conversation.py
@@ -8,8 +8,18 @@ import discord
 from . import BaseModel
 
 class Conversant(BaseModel):
-    user_id = peewee.BigIntegerField (null = False)
-    enabled = peewee.BooleanField    (null = False, default = True)
+    user_id              = peewee.BigIntegerField    (null = False)
+    enabled              = peewee.BooleanField       (null = False, default = True)
+    # active_conversation  = peewee.DeferredForeignKey ("Conversation", null = True)
+
+    @classmethod
+    def get_available(cls):
+        query = cls.select()
+        query = query.where(cls.enabled == True)
+        #TODO: turn this into one query
+        for conversant in query:
+            if Conversation.select_for(conversant, finished = True):
+                yield conversant
 
 class Conversation(BaseModel):
     conversant1     = peewee.ForeignKeyField (Conversant, null = False)
@@ -17,8 +27,11 @@ class Conversation(BaseModel):
     conversant2     = peewee.ForeignKeyField (Conversant, null = False)
     conversant2_key = peewee.TextField       (null = False, default = lambda : secrets.token_urlsafe(10))
     start_time      = peewee.DateTimeField   (null = False, default = lambda : datetime.datetime.utcnow())
-    end_time        = peewee.DateTimeField   (null = False)
+    end_time        = peewee.DateTimeField   (null = True)
     finished        = peewee.BooleanField    (null = False, default = False)
+
+    def get_other_conversant(self, current):
+         return self.conversant2 if self.conversant1 == current else self.conversant1
 
     @classmethod
     def select_for(cls, conversant, finished = None):
@@ -32,4 +45,3 @@ class Conversation(BaseModel):
         if finished is not None:
             query = query.where(cls.finished == finished)
         return query
-
