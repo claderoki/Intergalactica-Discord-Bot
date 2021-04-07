c9f6d4f2a22326a68487decf9b8ef8093ca7fd73
Claderoki <c.derhaag@protonmail.com>
2021-04-07 20:08:02 +0200

    fixed is_tester check

--- src/discord/helpers/checks.py
+++ src/discord/helpers/checks.py
@@ -1,9 +1,10 @@
 from discord.ext import commands
 from src.models import Human, database
+import src.config as config
 
 def is_tester():
     def predicate(ctx):
         with database.connection_context():
-            human = ctx.get_human()
+            human = config.bot.get_human(user = ctx.author)
             return human.tester
     return commands.check(predicate)
