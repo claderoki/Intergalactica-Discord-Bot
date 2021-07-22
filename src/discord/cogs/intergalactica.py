c82ea1f28f06129978d09a4a3942ec0eb830f887
Claderoki <49477117+claderoki@users.noreply.github.com>
2021-07-22 23:17:20 +0200

    bugfixes

--- src/discord/cogs/intergalactica.py
+++ src/discord/cogs/intergalactica.py
@@ -437,7 +437,7 @@ class Intergalactica(BaseCog):
             asyncio.gather(welcome_channel.send(embed = embed))
 
             self.last_member_join = datetime.datetime.utcnow()
-            if random.randint(0, 1000) == 0:
+            if random.randint(0, 250) == 0:
                 text = f"Hello and Welcome {member.mention}! My name is C-3PO and I’ll be around to assist you with anything you may need during your stay. I have prepared a fresh pigeon for you and can share many interesting facts about our server. I hope you enjoy your stay and I hope you have a wonderful evening."
             else:
                 text = f"Welcome {member.mention}! Make sure to pick some <#{self._channel_ids['roles']}> and make an <#{self._channel_ids['introductions']}>"
