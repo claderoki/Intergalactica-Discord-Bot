e0137cbff3b316d8869fffcf822b5c5ae174fa44
Claderoki <49477117+claderoki@users.noreply.github.com>
2021-09-14 14:37:44 +0200

    more cleanup

--- src/discord/cogs/personal.py
+++ src/discord/cogs/personal.py
@@ -1,4 +1,4 @@
-
+import datetime
 import asyncio
 
 import discord
@@ -6,17 +6,10 @@ from discord.ext import commands, tasks
 
 import src.config as config
 from src.discord.cogs.core import BaseCog
-
-def is_permitted():
-    def predicate(ctx):
-        authorized = ctx.author.id in (ctx.bot.owner.id, ctx.cog.user_id)
-        return authorized and (ctx.guild is None or ctx.guild.member_count < 10)
-    return commands.check(predicate)
+# from src.wrappers.hue_bridge import GetLightsCall, HueBridgeCall, HueBridgeCache, AuthenticateCall, Light
 
 class Personal(BaseCog):
-    @commands.Cog.listener()
-    async def on_ready(self):
-        pass
+    pass
 
 def setup(bot):
     bot.add_cog(Personal(bot))
