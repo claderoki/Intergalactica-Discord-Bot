c46458382c5f3d43f7cce17672f3773833919c15
Claderoki <49477117+claderoki@users.noreply.github.com>
2021-12-10 12:09:35 +0100

    more cleanup

--- src/discord/cogs/intergalactica.py
+++ src/discord/cogs/intergalactica.py
@@ -10,54 +10,15 @@ from dateutil.relativedelta import relativedelta
 
 from src.discord.cogs.custom.shared.helpers.helpers import ChannelHelper
 from src.discord.helpers.known_guilds import KnownGuild
-import src.config as config
 from src.discord.cogs.core import BaseCog
 from src.discord.errors.base import SendableException
-from src.discord.helpers.checks import specific_guild_only
 from src.discord.helpers.utility import get_context_embed
-from src.models import (Earthling, Human, Item,
-                        TemporaryVoiceChannel, TemporaryTextChannel, database)
+from src.models import (TemporaryVoiceChannel, TemporaryTextChannel, database)
 from src.discord.cogs.custom.shared.helpers.simple_poll import SimplePoll
 
-class MaliciousAction(Enum):
-    blacklisted_word = 1
-    invite_url       = 2
-    spam             = 3
-
-    @property
-    def ban_reason(self):
-        if self == self.blacklisted_word:
-            return "Using blacklisted word(s)"
-        elif self == self.invite_url:
-            return "Advertising"
-        elif self == self.spam:
-            return "Spam"
-
-async def on_malicious_action(action : MaliciousAction, member : discord.Member, **kwargs):
-    return
-
-    if action == MaliciousAction.blacklisted_word:
-        message = kwargs["message"]
-        words = kwargs["words"]
-
-        embed = await get_context_embed(message, amount = 5)
-        embed.color = discord.Color.red()
-        embed.set_author(name = f"Blacklisted word(s) used by {member} ({member.id})", url = message.jump_url)
-
-        lines = []
-        lines.append(f"The following blacklisted word(s) were used")
-        lines.append(", ".join([f"**{x}**" for x in words]))
-        embed.description = "{}\n{}".format("\n".join(lines), embed.description)
-
-    elif action == MaliciousAction.invite_url:
-        try:
-            await kwargs["message"].delete()
-        except: pass
-        ban_if_new = True
-    elif action == MaliciousAction.spam:
-        pass
-
 class Intergalactica(BaseCog):
+    _welcome_message_configs = {}
+
     last_member_join = None
 
     _role_ids = {
@@ -78,25 +39,6 @@ class Intergalactica(BaseCog):
         "c3po-log"       : 863775783940390912,
     }
 
-    async def get_invites(self, message):
-        if "cdn" in message:
-            return None
-
-        regex = re.compile(r'discord(?:app|\.gg)/(?:invite/)?([a-zA-Z0-9\-]{2,32})')
-        invite_urls = regex.findall(message)
-        if len(invite_urls) == 0:
-            return None
-
-        invites = []
-        for url in invite_urls:
-            try:
-                invite = await self.bot.fetch_invite(url)
-            except discord.errors.NotFound:
-                continue
-            else:
-                invites.append(invite)
-        return invites
-
     def get_channel(self, name):
         return self.bot.get_channel(self._channel_ids[name])
 
@@ -107,8 +49,6 @@ class Intergalactica(BaseCog):
     @commands.Cog.listener()
     async def on_ready(self):
         SimplePoll.add_guild_data(KnownGuild.intergalactica, self._channel_ids["staff_votes"], self._channel_ids["staff_chat"])
-
-        guild = self.bot.get_guild(KnownGuild.intergalactica)
 
         self.start_task(self.illegal_member_purger,             check = self.bot.production)
         self.start_task(self.introduction_purger,               check = self.bot.production)
@@ -122,21 +62,12 @@ class Intergalactica(BaseCog):
             if poll.should_finish():
                 poll.finish()
 
-    def blacklisted_words_used(self, text):
-        blacklisted_words = ["retard", "nigger", "fag"]
-
-        words_used = []
-        for word in blacklisted_words:
-            if word in text:
-                words_used.append(word)
-
-        return words_used
-
     @commands.Cog.listener()
     async def on_message(self, message):
-        if not self.bot.production:
-            return
         if message.guild is None:
+            return
+
+        if not self.bot.production:
             return
 
         guild_data = SimplePoll.guild_data.get(message.guild.id)
@@ -146,17 +77,6 @@ class Intergalactica(BaseCog):
             return
 
         if message.guild.id != KnownGuild.intergalactica:
-            return
-
-        words = self.blacklisted_words_used(message.content.lower())
-        if len(words) > 0:
-            await on_malicious_action(MaliciousAction.blacklisted_word, message.author, message = message, words = words)
-
-        invites = await self.get_invites(message.content)
-        if invites:
-            for invite in invites:
-                if invite.guild.id != message.guild.id:
-                    await on_malicious_action(MaliciousAction.invite_url, message.author, message = message)
             return
 
         if message.channel.id == self._channel_ids["general"]:
@@ -210,8 +130,7 @@ class Intergalactica(BaseCog):
             asyncio.gather(welcome_channel.send(embed = embed))
 
             self.last_member_join = datetime.datetime.utcnow()
-            if True:
-                text = f"Welcome {member.mention}! Make sure to pick some <#{self._channel_ids['roles']}> and make an <#{self._channel_ids['introductions']}>"
+            text = f"Welcome {member.mention}! Make sure to pick some <#{self._channel_ids['roles']}> and make an <#{self._channel_ids['introductions']}>"
             message = await self.get_channel("general").send(text)
             self.welcome_messages[member.id] = message
 
@@ -272,7 +191,7 @@ class Intergalactica(BaseCog):
     @tasks.loop(hours = 1)
     async def illegal_member_purger(self):
         for guild in self.bot.guilds:
-            if guild.id not in (KnownGuild.mouse, KnownGuild.intergalactica):
+            if guild.id not in (KnownGuild.mouse, ):
                 continue
 
             for member in guild.members:
