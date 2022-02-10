5f80b000673c47971a1cc95ba72af7c2329e477f
Claderoki <49477117+claderoki@users.noreply.github.com>
2022-02-10 22:17:55 +0100

    fixes

--- src/discord/helpers/colors.py
+++ src/discord/helpers/colors.py
@@ -7,15 +7,18 @@ import discord
 from src.wrappers.color_thief import ColorThief
 
 class ColorHelper:
-    def __get_dominant_color(url):
+    @classmethod
+    def __get_dominant_color(cls, url):
         color_thief = ColorThief(requests.get(url, stream=True).raw)
         dominant_color = color_thief.get_color(quality=1)
         return discord.Color.from_rgb(*dominant_color)
 
-    def get_dominant_color(self, guild = None) -> discord.Colour:
+    @classmethod
+    def get_dominant_color(cls, guild = None) -> discord.Colour:
         return discord.Color.from_rgb(242, 180, 37)
 
-    def get_random_color(self, start_range=80, end_range=255) -> discord.Colour:
+    @classmethod
+    def get_random_color(cls, start_range=80, end_range=255) -> discord.Colour:
         # from 80 to 255 because the majority of discord users use the dark theme, and anything under 80 is too bright to be comfortably visible.
         if end_range > 255:
             end_range = 255
