2286898957a10965236979c96015530bd364644f
Claderoki <c.derhaag@protonmail.com>
2022-03-16 15:33:38 +0100

    no message

--- src/discord/cogs/custom/cam/cog.py
+++ src/discord/cogs/custom/cam/cog.py
@@ -1,13 +1,10 @@
-import asyncio
-import datetime
-import random
-
 import discord
 from discord.ext import commands
 
-import src.config as config
+from src.discord.helpers import ColorHelper
 from src.discord.helpers.known_guilds import KnownGuild
 from ..shared.cog import CustomCog
+
 
 class KnownChannel:
     conspiracy = 905587705537265695
@@ -16,16 +13,18 @@ class KnownChannel:
 class KnownRole:
     conspiracy_redirector = 953386405185351721
 
+
 class KnownEmoji:
     ians_face = 852909058276458496
+
 
 class Cam(CustomCog):
     guild_id = KnownGuild.cam
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction: discord.Reaction, member: discord.Member):
-        # if not self.bot.production:
-        #     return
+        if self.bot.production:
+            return
 
         if isinstance(reaction.emoji, str):
             return
@@ -36,16 +35,29 @@ class Cam(CustomCog):
         if KnownRole.conspiracy_redirector not in [x.id for x in member.roles]:
             return
 
-        if reaction.message.channel.id == KnownChannel.conspiracy:
+        message = reaction.message
+        if message.channel.id == KnownChannel.conspiracy:
             return
 
         channel = member.guild.get_channel(KnownChannel.conspiracy)
-        await reaction.message.delete()
+        await message.delete()
 
-        embed = (discord.Embed(description=reaction.message.content)
-            .set_author(name = reaction.message.author.name, icon_url = reaction.message.author.avatar_url))
+        content = message.content if "youtube" in message.content else None
+        await channel.send(content, embed=message_to_embed(message))
 
-        await channel.send(embed = embed)
+
+def message_to_embed(message: discord.Message) -> discord.Embed:
+    embed = discord.Embed(color=ColorHelper.get_primary_color())
+    embed.set_author(name=message.author.name, icon_url=message.author.avatar_url)
+    embed.description = message.content
+    if "http" in message.content and ("png" in message.content or "jpg" in message.content):
+        embed.set_image(url=message.content)
+    else:
+        for attachment in message.attachments:
+            embed.set_image(url=attachment.url)
+            break
+
+    return embed
 
 
 def setup(bot):
